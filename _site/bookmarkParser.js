b4334c2f34d072f02300a54cd0f8d49ca0f76f19
Ben Fausch <ben@digitalbureau.com>
2017-06-06 12:05:37 -0600

    functioning parser

--- _site/bookmarkParser.js
+++ _site/bookmarkParser.js
@@ -1,6 +1,32 @@
-function getMeta() {
-    var container = '';
+container = 'var bookmarkData = [';
+var i = 0;
+var json_length = Object.keys(json_data).length;
+console.log(json_length)
+
+function getMeta(container) {
     console.log(json_data);
+    
+    //fetch metadata
+    for (jd in json_data) {
+        console.log(json_data[jd]['url'])
+        jdUrl = json_data[jd]['url'];
+        jdTitle = json_data[jd]['title'];
+        jdTime = json_data[jd]['dateAdded'];
+        jdImg = '';
+        jdDescription = '';
+        if (jdUrl) {
+            jdUrl = jdUrl.replace(/"/g, '\\"');
+            jdTitle = jdTitle.replace(/"/g, '\\"');
+            jdTime = jdTime.replace(/"/g, '\\"');
+            var url = encodeURIComponent(jdUrl);
+            fetchMeta(url, jdTitle, jdUrl, i);
+        }else{
+            i++
+        }
+    }
+};
+
+function fetchMeta(url, jdTitle, jdUrl) {
     var myHeaders = new Headers();
     var myInit = {
         method: 'GET',
@@ -8,59 +34,35 @@ function getMeta() {
         mode: 'cors',
         cache: 'default'
     };
-    container = 'var bookmarkData = [';
-    //fetch metadata
-    for (jd in json_data) {
-        // console.log(json_data[jd]['url'])
-        jdUrl = json_data[jd]['url'];
-        jdTitle = json_data[jd]['title'];
-        jdTime = json_data[jd]['dateAdded'];
-        jdImg = '';
-        jdDescription = '';
-        if (jdUrl) {
-            var url = encodeURIComponent(jdUrl);
-            fetch('https://opengraph.io/api/1.0/site/' + url + '?app_id=5934ae0ea6d559f93f3effe2', myInit).then((resp) => resp.json()).then(function(data) {
-                // console.log(data)
-                jdImg = data.hybridGraph.image;
-                jdDescription = data.hybridGraph.description;
-                container+='{"title": "'+jdTitle+'","url": "'+jdUrl+'","image": "'+jdImg+'","description": "'+jdDescription+'"},';
-                 // console.log(container)
-            })
-            // console.log(container)
+    fetch('https://opengraph.io/api/1.0/site/' + url + '?app_id=5935ae0ea6d549f93f3effe2', myInit).then((resp) => resp.json()).then(function(data) {
+        // console.log(data)
+        i++;
+        if (i === (json_length)) {
+            waitMeta(container)
         }
-    }
-    
-    console.log('done')
-    return container;
-};
+        jdImg = data.hybridGraph.image;
+        jdDescription = data.hybridGraph.description;
+        jdImg = jdImg.replace(/"/g, '\\"');
+        jdDescription = jdDescription.replace(/"/g, '\\"');
+        container = container + '{"title": "' + jdTitle + '","url": "' + jdUrl + '","image": "' + jdImg + '","description": "' + jdDescription + '"},';
+        console.log(i)
+        container = container + ' {"title": "' + jdTitle + '","url": "' + jdUrl + '"},';
+    })
+}
 
-
-async function waitMeta(){
-var theMeta = await getMeta();
-console.log('METAMETAMETAMETAMETA')
-
-console.log(theMeta)
-
-
-
+function waitMeta(container) {
+    console.log('METAMETAMETAMETAMETA')
     //create downloadble file
+    container = container.replace(/\s+/g, " ");
+    container = container + '];'
     var textFileAsBlob = new Blob([container], {
         type: 'text/plain'
     });
-
     var downloadLink = document.createElement("a");
     downloadLink.download = 'bookmarks.json';
     downloadLink.innerHTML = "Download File";
     downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
     document.getElementById('container').appendChild(downloadLink)
 }
-waitMeta();
+getMeta(container);
 // window.onload = waitMeta();
-
-
-
-
-
-
-
-
